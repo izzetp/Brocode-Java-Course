e21bc47cd48feff2813eb031ae22fa7e4a70382b
izzetp <135380394+izzetp@users.noreply.github.com>
2025-03-08 23:15:35 +0100

    Create 1.-Introduction-to-java

---
+++ 1.-Introduction-to-java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("I like pizza!");
+
+        // comment
+
+        /*
+        comment
+        */
+
+        System.out.println("Hello");
+        System.out.println("Hey");
+        System.out.println("What's up?");
+    } 
+}
