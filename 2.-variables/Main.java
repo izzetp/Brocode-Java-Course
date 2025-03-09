9784086164d5a497e2edc5ea4185963a9dd5e7d2
izzetp <135380394+izzetp@users.noreply.github.com>
2025-03-09 23:38:39 +0100

    Create Main.java

---
+++ 2.-variables/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main (String[] args) {
+
+        int age = 21;
+        int year = 2025;
+
+        double price = 19.99;
+        double gpa = 3.5;
+
+        char grade = 'A';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+
+        String name = "Bro code";
+        String food = "pizza";
+
+    }
+}
