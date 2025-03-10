d2e310d161970d2075203792de60650c1f48a4ce
izzetp <135380394+izzetp@users.noreply.github.com>
2025-03-10 13:58:30 +0100

    Multiple Changes
    Create Input.java
    Create Area.java
    Rename Main.java
    Rename Main.java
    Rename Main.java
    Update README.md

---
+++ 3.-user-input/Area.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+
+        // calculate the area of a rectangle
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+
+        System.out.println("The area is: " + area + "cm^2");
+
+        scanner.close();
+    }
+}
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 * 08/03/25 Day 1: 0:00-10:58
 * 09/03/25 Day 2: 10:58-31:30
+* 10/03/25 Day 3: 31:30-47:25
 
 * https://www.youtube.com/watch?v=xTtL8E4LzTQ&t=1890s
 * https://www.youtube.com/watch?app=desktop&v=ZIX1m-bYJyU
