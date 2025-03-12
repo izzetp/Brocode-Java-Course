3c0b304373d844a1f92a4b2e0ef2e5f14acb3ff9
izzetp <135380394+izzetp@users.noreply.github.com>
2025-03-12 18:45:04 +0100

    Update Arithmetic.java

--- 5.-arithmetic/Arithmetic.java
+++ 5.-arithmetic/Arithmetic.java
@@ -3,13 +3,37 @@ public class Arithmetic {
 
         // Arithmetic operators
 
-        int x = 10;
-        int y = 2;
-        int z;
+        // int x = 10;
+        // int y = 2;
+        // int z;
 
-        z = x + y;
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x / y;
+        // z = x % y;
 
+        // System.out.println(z);
 
-        System.out.println(z);
+        // x += y;
+        // x -= y;
+        // x *= y;
+        // x /= y;
+        // x %= y;
+
+        // System.out.println(x);
+
+        int x = 1;
+
+        x++;
+        x--;
+
+        System.out.println(x);
+
+        // order of operations [p-e-m-d-a-s]
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+
+        System.out.println(result);
     }
 }
