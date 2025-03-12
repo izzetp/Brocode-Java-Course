d7ece86474e946d70c81326edba38e5ef222b10e
izzetp <135380394+izzetp@users.noreply.github.com>
2025-03-12 18:31:39 +0100

    Create Arithmetic.java

---
+++ 5.-arithmetic/Arithmetic.java
@@ -0,0 +1,15 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+
+        // Arithmetic operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        z = x + y;
+
+
+        System.out.println(z);
+    }
+}
