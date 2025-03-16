11858d0a1b0af1315e8b9b71d530575289b211e8
izzetp <135380394+izzetp@users.noreply.github.com>
2025-03-16 12:41:21 +0100

    update Math_Class.java

---
+++ 9.-math-class/Math_Class.java
@@ -0,0 +1,14 @@
+public class Math {
+    public static void main(String[] args) {
+
+        // System.out.println(Math.PI);
+        // System.out.println(Math.E);
+
+        double result;
+
+        result = Math.pow(2, 3);
+
+        System.out.println(result);
+
+    }
+}
