be719537e17a36050eeaf4a3645747d841cbed9d
izzetp <135380394+izzetp@users.noreply.github.com>
2025-03-17 23:23:17 +0100

    Update 10.-printf-func

---
+++ 10.-printf-func/Precision.java
@@ -0,0 +1,13 @@
+public class Precision {
+    public static void main(String[] args) {
+
+        double price1 = 9.99;
+        double price2 = 100.15;
+        double price3 = -54.01;
+
+        System.out.printf("%,.1f\n", price1);
+        System.out.printf("%+.2f\n", price2);
+        System.out.printf("%(.3f\n", price3);
+
+    } 
+}
