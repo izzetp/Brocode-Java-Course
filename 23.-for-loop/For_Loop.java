3d45daa0325756fa85b031b416017ecf1fd2e497
izzetp <135380394+izzetp@users.noreply.github.com>
2025-03-30 18:15:32 +0200

    Update For_Loop.java

--- 23.-for-loop/For_Loop.java
+++ 23.-for-loop/For_Loop.java
@@ -8,9 +8,18 @@ public class For_Loop {
         System.out.print("Enter how many times you want to loop: ");
         int max = scanner.nextInt();
 
-        for (int i = 0: i < max; i++) {
-            System.out.println("");
+        for (int i = 0; i < max; i++) {
+            System.out.println(i);
         }
+
+        System.out.print("How many seconds to countdown from?: ");
+        int start = scanner.nextInt();
+
+        for (int i = start; i > 0; i--) {
+            System.out.println(i);
+        }
+        
+        System.out.println("HAPPY NEW YEAR!");
 
         scanner.close();
     }
