3da100c40eeee8e916f7bbd761b90c17883b4d15
izzetp <135380394+izzetp@users.noreply.github.com>
2025-04-02 18:54:42 +0200

    Create Methods2.java

---
+++ 26.-methods/Methods2.java
@@ -0,0 +1,10 @@
+public class Methods2 {
+    public static void main(String[] args) {
+        
+        double result = square(3);
+        System.out.println(result);
+    }
+
+    static double square(double number) {
+        return number * number;
+}
