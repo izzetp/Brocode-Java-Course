922fc1c3212fc9c0fefda0b7d73c7ee4ad7a3d75
izzetp <135380394+izzetp@users.noreply.github.com>
2025-04-04 01:45:36 +0200

    Update Overloaded_Methods.java

--- 28.-overloaded-methods/Overloaded_Methods.java
+++ 28.-overloaded-methods/Overloaded_Methods.java
@@ -4,6 +4,7 @@ public class Overloaded_Methods {
         System.out.println(add(1,2,3));
 
         String pizza = bakePizza("flat bread", "mozzarella");
+        System.out.println(pizza);
     }
     static double add(double a, double b) {
         return a + b;
@@ -13,9 +14,10 @@ public class Overloaded_Methods {
         return a + b + c;
     }
 
-    static String bakePizzaString(String bread) {
+    static String bakePizza(String bread) {
         return bread + " pizza";
     }
-    static String bakePizzaString(String bread, String cheese) {
+    static String bakePizza(String bread, String cheese) {
         return cheese + " " + bread + " pizza";
+    }
 }
