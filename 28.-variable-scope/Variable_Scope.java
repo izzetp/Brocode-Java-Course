821b125bb7590fe4584c8340784b7dc27ce16229
izzetp <135380394+izzetp@users.noreply.github.com>
2025-04-04 15:05:53 +0200

    Update Variable_Scope.java

--- 28.-variable-scope/Variable_Scope.java
+++ 28.-variable-scope/Variable_Scope.java
@@ -1,15 +1,17 @@
 public class Variable_Scope {
 
-    static int x = 3;
+    static int x = 3; //class
     public static void main(String[] args) {
         
-        int x = 1;
+        int x = 1; //local
 
         System.out.println(x);
+
+        doSomething();
     }
 
     static void doSomething() {
-        int x = 2;
+        int x = 2; // local
 
         System.out.println(x);
     }
