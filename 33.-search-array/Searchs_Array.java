b80192c19936479c67eb5a9b9777f7b3f9c6e67c
izzetp <135380394+izzetp@users.noreply.github.com>
2025-04-10 14:35:25 +0200

    Update Searchs_Array.java

--- 33.-search-array/Searchs_Array.java
+++ 33.-search-array/Searchs_Array.java
@@ -1,17 +1,34 @@
-
+import java.util.Scanner;
 
 public class Searchs_Array {
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
 
         int numbers[] = {1, 9, 2, 8, 3, 5, 4};
-        int target = 2;
+        String[] fruits = {"apple", "orange", "banana"};
+        boolean isFound = false;
+        String target;
 
-        for (int i = 0 ; i < numbers.length; i++) {
-            if (target == numbers[i]) {
+        System.out.print("Enter a fruit to search for: ");
+        target = scanner.nextLine();
+
+        // String target = "apple";
+        // int target2 = 2;
+        
+        for (int i = 0 ; i < fruits.length; i++) {
+            if (fruits[i].equals(target)) {
                 System.out.println("Element found at index: " + i);
+                isFound = true;
+                break;
             }
         }
+
+        if (!isFound) {
+            System.out.println("Elememnt not found in the array.");
+        }
+
+        scanner.close();
 
 
     }
