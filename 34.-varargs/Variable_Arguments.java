d1dcc5d6c337e663546c3c6cd4d5e40cacd9a63b
izzetp <135380394+izzetp@users.noreply.github.com>
2025-04-11 16:00:29 +0200

    Update Variable_Arguments.java

--- 34.-varargs/Variable_Arguments.java
+++ 34.-varargs/Variable_Arguments.java
@@ -3,9 +3,33 @@
 public class Variable_Arguments {
     public static void main(String[] args) {
         
+        System.out.println(add(1, 2, 3, 4, 5, 6));
+        System.out.println(average(1, 2, 3, 4, 5, 6));
 
     }
     static int add(int... numbers) {
         
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        return sum;
+
+    }
+    static double average(double... numbers) {
+
+        double sum = 0;
+
+        if (numbers.length == 0) {
+            return 0;
+        }
+
+        for (double number : numbers) {
+            sum += number;
+        }
+
+        return sum / numbers.length;
     }
 }
