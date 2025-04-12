34302ae0dfe411dcccce31423560801fcb89564c
izzetp <135380394+izzetp@users.noreply.github.com>
2025-04-12 19:58:32 +0200

    Update ThreeD_Arrays.java

---
+++ 35.-3d-arrays/ThreeD_Arrays.java
@@ -0,0 +1,19 @@
+public class ThreeD_Arrays {
+    public static void main(String[] args) {
+
+        //String[] fruit = {"apple", "orange", "banana"};
+        //String[] vegtables = {"potato", "onion", "carrot"};
+        //String[] meats = {"chicken", "pork", "beef", "fish"};
+
+        String[][] groceries = {{"apple", "orange", "banana"},
+                                {"potato", "onion", "carrot"},
+                                {"chicken", "pork", "beef", "fish"}};
+
+        for (String[] foods : groceries) {
+            for (String food : foods) {
+                System.out.print(food + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
