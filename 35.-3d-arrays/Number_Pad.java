9051944861cb8d6ea1ae8b2e26e1713cf573f84a
izzetp <135380394+izzetp@users.noreply.github.com>
2025-04-14 03:26:46 +0200

    Update Number_Pad.java

--- 35.-3d-arrays/Number_Pad.java
+++ 35.-3d-arrays/Number_Pad.java
@@ -3,9 +3,9 @@
 public class Number_Pad {
     public static void main(String[] args) {
         
-        char [][] telephone = {{'1', '2', '3'}
-                               {'4', '5', '6'}
-                               {'7', '8', '9'}
+        char [][] telephone = {{'1', '2', '3'},
+                               {'4', '5', '6'},
+                               {'7', '8', '9'},
                                {'*', '0', '#'}};
 
         for (char[] row : telephone) {
