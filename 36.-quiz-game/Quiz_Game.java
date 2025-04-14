672813b7cdf5f946dcc66e4394b0383fb54ecdf1
izzetp <135380394+izzetp@users.noreply.github.com>
2025-04-14 15:47:59 +0200

    Create Quiz_Game.java

---
+++ 36.-quiz-game/Quiz_Game.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Quiz_Game {
+    public static void main(String[] args) {
+        
+        // questions array[]
+        String[] questions = {"What is the main function of a router?",
+                              "Which part of hte computer is considered the brain?",
+                              "What year was Facebook launced?",
+                              "Who is known as the father of computers?",
+                              "What was the first programming langage?"};
+
+        // options array[][]
+        String[][] options = {{"Storing files", "2. Encrpyting data", "3. directing internet raffic", "4. Managing passwords"},
+                              {"1. CPU", "2. Hard Drive", "3. RAM", "4. GPU"},
+                              {"1. 2000", "2. 2004", "3. 2006", "4. 2008"},
+                              {"1. Steve Jobs", "2. Bill GAtes", "3. Alan Turing", "4. Charles Babbage"},
+                              {"1. COBOL", "2. C", "3. Fortan", "4. Assembly"}};
+
+        // declar variables
+        int[] answers = {3, 1, 2, 4, 3};
+
+        // welcome message
+
+        // question (loop)
+        //options
+        // get guess
+        // check guess
+
+        // display score
+    }
+}
