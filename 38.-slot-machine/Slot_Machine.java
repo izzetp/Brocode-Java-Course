45e472c81ffaa94421ccfc855e6140e08b1fdcbd
izzetp <135380394+izzetp@users.noreply.github.com>
2025-04-20 04:47:34 +0200

    Update Slot_Machine.java

--- 38.-slot-machine/Slot_Machine.java
+++ 38.-slot-machine/Slot_Machine.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class Slot_Machine {
     public static void main(String[] args) {
         
-        
+        // declare
+
+        Scanner scanner = new Scanner(System.in);
+        int balance = 100;
+        int bet;
+        int payout;
+        String[] row;
+
+        System.out.println("***************************");
+        System.out.println("   Welcome to Java Slots   ");
+        System.out.println("Symbols: 🍒 🍉 🍋 🔔 ⭐  ");
+        System.out.println("***************************");
+
+
+        // welcome
+        // play if balance greater zero
+        // enter bet amount
+        // verify if bet > balance
+        // verify if bet > 0
+        // subtract bet
+        // spin row
+        //print row
+        // get pay out
+        // ask play again
+        //display message
     }
 }
