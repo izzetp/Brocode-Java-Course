8ee2ccba5d3fd595d225685b855c071efe73b974
izzetp <135380394+izzetp@users.noreply.github.com>
2025-04-27 01:21:49 +0200

    Update Constructors.java

--- 40.-constructors/Constructors.java
+++ 40.-constructors/Constructors.java
@@ -1,8 +1,17 @@
 
 public class Constructors {
     public static void main(String[] args) {
-        Student student1 = new Student();
-        Student student2 = new Student();
+        Student student1 = new Student("Spongebob", 30, 3.2);
+        Student student2 = new Student("Patrick", 34, 1.5);
+
+        System.out.println(student1.name);
+        System.out.println(student1.age);
+        System.out.println(student1.gpa);
+
+        System.out.println(student2.name);
+        System.out.println(student2.age);
+        System.out.println(student2.gpa);
+
 
     }
 }
