ff92e4943253bbc980f239efbf800c837583f5ab
izzetp <135380394+izzetp@users.noreply.github.com>
2025-04-28 10:40:21 +0200

    Update README.md

--- 41.-overloaded-constructor/User.java
+++ 41.-overloaded-constructor/User.java
@@ -2,8 +2,14 @@ public class User {
     
     String username;
     String email;
-    in age;
+    int age;
 
+    User() {
+        this.username = "Guest";
+        this.email = "Not provided";
+        this.age = 0;
+    }
+    
     User(String username) {
         this.username = username;
         this.email = "Not provided";
@@ -11,5 +17,18 @@ public class User {
 
     }
 
+    User(String username, String email) {
+        this.username = username;
+        this.email = email;
+        this.age = 0;
+
+    }
+    
+    User(String username, String email, int age) {
+        this.username = username;
+        this.email = email;
+        this.age = age;
+
+    }
 
 }
