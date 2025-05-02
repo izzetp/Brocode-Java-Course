c55172dad52fffecb481651ee57f598334d0885c
izzetp <135380394+izzetp@users.noreply.github.com>
2025-05-02 14:02:34 +0200

    Update Dog.java

---
+++ 44.-inheritancee/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    
+    boolean isAlive;
+
+    Animal () {
+        isAlive = true;
+    }
+
+    void eat() {
+        System.out.println("This animal is eating.");
+    }
+}
