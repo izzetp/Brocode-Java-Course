19bcdd8604cb79688c97608ddc4f0a6bcc88d43d
izzetp <135380394+izzetp@users.noreply.github.com>
2025-05-04 01:40:21 +0200

    Create Inheritancee.java

---
+++ 44.-inheritancee/Inheritancee.java
@@ -0,0 +1,11 @@
+public class Inheritancee {
+    public static void main(String[] args) {
+        
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.eat();
+        cat.eat();
+        
+    }
+}
