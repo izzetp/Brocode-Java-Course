92a1aada58eddae13006d351fbe541128064cf6b
izzetp <135380394+izzetp@users.noreply.github.com>
2025-05-04 18:57:44 +0200

    Update Supper.java

---
+++ 45.-superr/Studentt.java
@@ -0,0 +1,10 @@
+public class Studentt extends Personn{
+    
+    double gpa;
+
+    Studentt(String first, String last, double gpa) {
+        super(first, last);
+        this.gpa = gpa;
+
+    }
+}
--- 45.-superr/superr.java
+++ 45.-superr/superr.java
@@ -2,7 +2,10 @@ public class Superr {
     public static void main(String[] args) {
         
         Personn person = new Personn("Tom", "Riddle");
+        Studentt student = new Studentt("Harry", "Potter", 3.25);
 
         person.showName();
+        System.out.println(student.gpa);
+
     }
 }
