16476b5dbfa7f1c84cdbe06a4959b42fcd466d06
izzetp <135380394+izzetp@users.noreply.github.com>
2025-05-07 18:57:26 +0200

    Update Tostring_Methodd.java

---
+++ 47.-tostring-method/Tostring_Methodd.java
@@ -0,0 +1,11 @@
+
+
+public class Tostring_Methodd {
+    public static void main(String[] args) {
+        Carrr car = new Carrr("Ford", "Mustang", 2025, "Red");
+        Carrr car1 = new Carrr("Chevrolet", "Corvette", 2026, "Blue");
+
+        System.out.println(car);
+        System.out.println(car1);
+    }
+}
