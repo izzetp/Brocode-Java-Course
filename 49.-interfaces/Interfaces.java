57056675cfc3c0e4b46a77df552db6963e097cf9
izzetp <135380394+izzetp@users.noreply.github.com>
2025-05-10 01:17:37 +0200

    Update Interfaces.java

---
+++ 49.-interfaces/Interfaces.java
@@ -0,0 +1,14 @@
+public class Interfaces {
+    public static void main(String[] args) {
+        
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
+        Fish fish = new Fish();
+
+        rabbit.flee();
+        hawk.hunt();
+        fish.flee();
+        fish.hunt();
+
+    }
+}
