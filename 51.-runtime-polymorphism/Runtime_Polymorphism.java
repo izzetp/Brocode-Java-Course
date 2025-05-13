e4bd88a270117ecfa27ab03ebe7747cc564c84f4
izzetp <135380394+izzetp@users.noreply.github.com>
2025-05-13 21:07:45 +0200

    Update Runtime_Polymorphism.java

--- 51.-runtime-polymorphism/Runtime_Polymorphism.java
+++ 51.-runtime-polymorphism/Runtime_Polymorphism.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Runtime_Polymorphism {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         
-        
+        Animal51 animal;
+
+        System.out.print("Would you like a dog or a cat? (1 = dog, 2 = cat): ");
+
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            animal = new Dog51();
+            animal.speak();
+        } else if (choice == 2) {
+            animal = new Cat51();
+            animal.speak();
+        }
     }
 }
