79078a6169f638ef96693f3e5e7f7a3823f0ab64
izzetp <135380394+izzetp@users.noreply.github.com>
2025-05-14 08:33:36 +0200

    Update Car52.java

--- 52.-getters-setters/Car52.java
+++ 52.-getters-setters/Car52.java
@@ -1,12 +1,24 @@
 public class Car52 {
     
-    String model;
-    String color;
-    int price;
+    private String model;
+    private String color;
+    private int price;
 
     Car52 (String model, String color, int price) {
         this.model = model;
         this.color = color;
         this.price = price;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+    String getColor() {
+        return this.color;
+    }
+    
+    String getPrice() {
+        return this.price;
     }
 }
