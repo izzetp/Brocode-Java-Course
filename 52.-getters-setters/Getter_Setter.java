7a71cad1c8c40b803e499d2cb2a0523df119aa25
izzetp <135380394+izzetp@users.noreply.github.com>
2025-05-14 08:33:27 +0200

    Update Getter_Setter.java

---
+++ 52.-getters-setters/Getter_Setter.java
@@ -0,0 +1,10 @@
+public class Getter_Setter {
+    public static void main(String[] args) {
+        
+        Car52 car = new Car52("Charger", "Yellow", 10000);
+
+
+
+        System.out.println(car.getColor + " " + car.getModel + " " + car.getPrice);
+    }
+}
