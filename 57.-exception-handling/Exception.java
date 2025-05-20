829a1c130676dc75ae67e36b65a70ee71d39013e
izzetp <135380394+izzetp@users.noreply.github.com>
2025-05-20 16:31:35 +0200

    Update Exception.java

--- 57.-exception-handling/Exception.java
+++ 57.-exception-handling/Exception.java
@@ -1,16 +1,17 @@
 import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Exception {
     public static void main(String[] args) {
-        
-        try {
-            System.out.println("Enter a number: ");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+            System.out.println(number);
         } catch (InputMismatchException e) {
             System.out.println("That was not a number!");            
         } catch (ArithmeticException e) {
             System.out.println("You can't divide by zero!");
-        }
-
-
+        } 
     }
 }
