49fc19c32cb4f9a3a9272684644ba259f6c41588
izzetp <135380394+izzetp@users.noreply.github.com>
2025-05-22 08:58:07 +0200

    Update Write_Files.java

--- 58.-write-files/Write_Files.java
+++ 58.-write-files/Write_Files.java
@@ -1,11 +1,12 @@
-import java.io.FilterWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Write_Files {
     public static void main(String[] args) {
 
-        try(FileWriter writer = new FileWriter("test.txt");) {
+        try(FileWriter writer = new FileWriter("test.txt")) {
             writer.write("I like pizza!");
+            System.out.println("File has been written");
         } catch(IOException e) {
             System.out.println("Could not write file");
         }
