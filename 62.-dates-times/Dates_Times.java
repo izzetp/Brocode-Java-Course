9623507213c79c5f8bce0063a2f698604edc555e
izzetp <135380394+izzetp@users.noreply.github.com>
2025-06-05 11:28:57 +0200

    Update Dates_Times.java

--- 62.-dates-times/Dates_Times.java
+++ 62.-dates-times/Dates_Times.java
@@ -1,10 +1,16 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Dates_Times {
     public static void main(String[] args) {
         
         LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+        LocalDateTime dateTime = LocalDateTime.now();
 
         System.out.println(date);
+        System.out.println(time);
+        System.out.println(dateTime);
     }
 }
