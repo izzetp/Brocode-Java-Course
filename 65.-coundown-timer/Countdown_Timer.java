6bc002da1348a25d286273ec5d24baebc8f9b60c
izzetp <135380394+izzetp@users.noreply.github.com>
2025-06-09 11:40:04 +0200

    Update Countdown_Timer.java

--- 65.-coundown-timer/Countdown_Timer.java
+++ 65.-coundown-timer/Countdown_Timer.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Countdown_Timer {
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter # of seconds to countdown from: ");
+        int response = scanner.nextInt(); 
+
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             
-            int count = 5;
+            int count = response;
 
             @Override
             public void run() {
