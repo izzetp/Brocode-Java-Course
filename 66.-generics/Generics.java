a835415a44594b8e00bf85f2f5d5934d46b81fbf
izzetp <135380394+izzetp@users.noreply.github.com>
2025-06-11 11:32:51 +0200

    Update Generics.java

--- 66.-generics/Generics.java
+++ 66.-generics/Generics.java
@@ -3,7 +3,13 @@ import java.util.ArrayList;
 public class Generics {
     public static void main(String[] args) {
 
-        Product<String, Double> product = new Product<>("Apple", 0.50);
+        Product<String, Double> product1 = new Product<>("Apple", 0.50);
+        Product<String, Integer> product2 = new Product<>("Ticket", 15);
+
+
+        System.out.println(product1.getItem());
+        System.out.println(product1.getPrice());
+
 
 
 
