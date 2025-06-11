50b466007109506c270e9ddf24d5502a46a1cbf2
izzetp <135380394+izzetp@users.noreply.github.com>
2025-06-11 11:24:17 +0200

    Update Generics.java

--- 66.-generics/Generics.java
+++ 66.-generics/Generics.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+
 public class Generics {
     public static void main(String[] args) {
         
-        
+        Box<String> box = new Box<>();
+
+        box.setItem("banana");
+
+        System.out.println(box.getItem());
+
     }
 }
