02403324378941e5d64d7b094a43e073c4fc0468
izzetp <135380394+izzetp@users.noreply.github.com>
2025-06-12 15:45:49 +0200

    Update Hashmaps.java

--- 67.-hashmaps/Hashmaps.java
+++ 67.-hashmaps/Hashmaps.java
@@ -6,8 +6,11 @@ public class Hashmaps {
         HashMap<String, Double> map = new HashMap<>();
 
         map.put("apple", 0.50);
-        map.put("banana", 0.75);
+        map.put("orange", 0.75);
         map.put("banana", 0.25);
+        map.put("coconut", 1.00);
+
+        // map.remove("apple");
 
         System.out.println(map);
     }
