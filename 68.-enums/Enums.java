01db6529949db2cdfbd2077f08899a8c0ebe1fdd
izzetp <135380394+izzetp@users.noreply.github.com>
2025-06-14 17:34:57 +0200

    Update Enums.java

--- 68.-enums/Enums.java
+++ 68.-enums/Enums.java
@@ -1,11 +1,23 @@
-
+import java.util.Scanner;
 
 public class Enums {
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a day of the week: ");
+        String response = scanner.nextLine().toUpperCase();
+
         Day day = Day.SUNDAY;
 
-        System.out.println(day);
-        System.out.println(day.getDayNumber());
+        switch (day) {
+            case MONDAY,
+                 TUESDAY,
+                 WEDNESDAY,
+                 THURSDAY,
+                 FRIDAY -> System.out.println("It is a weekday");
+            case SATURDAY, SUNDAY -> System.out.println("It is a weekend");
+        }
+
+        scanner.close();
     }
 }
