77768cf802d4c4e99e98b3075a209711b263fc9d
izzetp <135380394+izzetp@users.noreply.github.com>
2025-06-18 10:49:20 +0200

    Update Multithreading.java

---
+++ 70.-multithreading/Multithreading.java
@@ -0,0 +1,8 @@
+public class Multithreading {
+    public static void main(String[] args) {
+        
+        MyRunnablee myrunnble = new MyRunnablee();
+        Thread thread = new Thread();
+        thread.start();
+    }
+}
