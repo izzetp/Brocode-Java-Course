c4781d01d093e8d7f01433153e7b13767bd39279
izzetp <135380394+izzetp@users.noreply.github.com>
2025-06-18 10:49:12 +0200

    Update MyRunnablee.java

---
+++ 70.-multithreading/MyRunnablee.java
@@ -0,0 +1,14 @@
+public class MyRunnablee implements Runnablee {
+    
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(1000);
+                System.out.println(i);
+            } catch (InterruptedException e) {
+                System.out.println("Thread was interrupted");
+            }
+        }
+    }
+}
