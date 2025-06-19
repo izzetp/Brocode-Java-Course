9aac663a8e2d86f179515517292b5a2576defaf9
izzetp <135380394+izzetp@users.noreply.github.com>
2025-06-19 11:33:09 +0200

    Update MyRunnablee.java

--- 70.-multithreading/Multithreading.java
+++ 70.-multithreading/Multithreading.java
@@ -1,8 +1,23 @@
+
 public class Multithreading {
     public static void main(String[] args) {
         
-        MyRunnablee myRunnable = new MyRunnablee();
-        Thread thread = new Thread(new myRunnable);
-        thread.start();
+        Thread thread1 = new Thread(new MyRunnablee("PING"));
+        Thread thread2 = new Thread(new MyRunnablee("PONG"));
+
+        System.out.println("GAME START!");
+
+        thread1.start();
+        thread2.start();
+        
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread was interrupted");
+        }
+        
+
+        System.out.println("GAME OVER!");
     }
 }
--- 70.-multithreading/MyRunnablee.java
+++ 70.-multithreading/MyRunnablee.java
@@ -1,11 +1,19 @@
+
+
 public class MyRunnablee implements Runnable {
     
+    private final String text;
+
+    MyRunnablee(String text) {
+        this.text = text;
+    }
+
     @Override
     public void run() {
         for (int i = 1; i <= 5; i++) {
             try {
                 Thread.sleep(1000);
-                System.out.println(i);
+                System.out.println(text);
             } catch (InterruptedException e) {
                 System.out.println("Thread was interrupted");
             }
