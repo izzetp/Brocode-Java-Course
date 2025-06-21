4a9fbdfe8bccfba16f39592c8e8c0068720b45f5
izzetp <135380394+izzetp@users.noreply.github.com>
2025-06-21 13:12:26 +0200

    Update Alarm_Clock.java

--- 71.-alarm-clock/Alarm_Clock.java
+++ 71.-alarm-clock/Alarm_Clock.java
@@ -8,17 +8,23 @@ public class Alarm_Clock {
         
         Scanner scanner = new Scanner(System.in);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTime alarmTime;
+        LocalTime alarmTime = null;
 
-        try {
-            System.out.println("Enter an alarm time (HH:MM:SS): ");
-            String inputTime = scanner.nextLine();
+        while (alarmTime == null) {
+            try {
+                System.out.println("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = scanner.nextLine();
 
-            alarmTime = LocalTime.parse(inputTime, formatter);
-            System.out.println("Alarm set for " + alarmTime);
-        } catch (DateTimeParseException e) {
-            System.out.println("Invalid format. Please use HH:MM:SS");
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for " + alarmTime);
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid format. Please use HH:MM:SS");
+            }
         }
+
+        AlarmClock alarmClock = new AlarmClock(alarmTime);
+        Thread alarmThread = new Thread(alarmClock);
+        alarmThread.start();
 
         scanner.close();
     }
